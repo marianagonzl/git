47c771c473ba21f62d156cb8b1a81f374522adc0
Mariana Gonzalez <10193@portalcedi.edu.mx>
2024-01-30 13:44:58 -0600

    modificaciones

--- holapapu.cpp
+++ holapapu.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+        int edad;
         cout << " whats up homies" << endl;
+        cout << "Escribe tu edad" << endl;
+        cin >> edad;
+        cout << edad;
+
+
     return 0;
 }
